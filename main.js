b4b3e1e32747c2791bdce9dc46d4d1872d1ff110
Mees Gelein <meesgelein@gmail.com>
2018-05-22 13:50:43 +0200

    Removed the menubar

--- main.js
+++ main.js
@@ -7,14 +7,16 @@ const url = require('url')
  */
 function createWindow() {
     // Create the browser window.
-    win = new BrowserWindow({ width: 1280, height: 720 })
+    win = new BrowserWindow({ width: 1280, height: 720, minWidth: 640, minHeight: 360 });
 
     // and load the index.html of the app.
     win.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
         slashes: true
-    }))
+    }));
+    //Remove the top menu bar
+    win.setMenu(null);
 }
 
 app.on('ready', createWindow)
